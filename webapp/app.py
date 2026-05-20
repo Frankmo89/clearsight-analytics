a700dd95abf6373daf7479de4ad92cecc7a6dc4b
Frankmo89 <alonsosky617@gmail.com>
2026-05-20 16:42:48 -0700

    fix: set OMP/KMP env vars before imports to prevent TF/PyTorch OpenMP segfault

--- webapp/app.py
+++ webapp/app.py
@@ -9,6 +9,12 @@ Run: streamlit run webapp/app.py   (from project root)
 ================================================================================
 """
 
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["TF_NUM_INTRAOP_THREADS"] = "1"
+os.environ["TF_NUM_INTEROP_THREADS"] = "1"
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 import sys
 import warnings
 warnings.filterwarnings("ignore")
